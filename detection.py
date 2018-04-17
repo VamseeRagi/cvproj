12b33581190407d16c5007670e325a0b8268798b
AmeerD <36680113+AmeerD@users.noreply.github.com>
2018-04-17 19:20:05 -0400

    i
    i

--- detection.py
+++ detection.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 
-def get_bounding_boxes(file_path_and_name):
+def get_bounding_boxes(img):
     face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    img = cv.imread(file_path_and_name)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 6)
     return faces
